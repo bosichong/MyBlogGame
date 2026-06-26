d6e3d7898a52c167607bdbbed6f36e53764e6aa6
bosichong <bosichong@qq.com>
2026-06-27 07:56:19 +0800

    feat: 提名/入围时播放撒花动画

--- 优秀博客大奖赛/jiexiao_web.gd
+++ 优秀博客大奖赛/jiexiao_web.gd
@@ -1,5 +1,6 @@
 extends Node2D
 const SCENE_PATH = "res://scenes/main.tscn"
+const CONFETTI_SCENE = preload("res://scenes/confetti.tscn")
 
 const YEAR_MAP = {
     1: 2005, 2: 2010, 3: 2015, 4: 2020, 5: 2025,
@@ -8,6 +9,13 @@ const YEAR_MAP = {
 func _ready() -> void:
     _update_content()
 
+func _play_confetti_if_celebrated(award_result_value: int) -> void:
+    if award_result_value in [AwardManager.AwardResult.NOMINATE, AwardManager.AwardResult.SHORTLIST]:
+        var confetti = CONFETTI_SCENE.instantiate()
+        confetti.auto_emit = false
+        add_child(confetti)
+        confetti.emit()
+
 func _update_content() -> void:
     var player_name: String = str(Blogger.blog_data.get("blog_name", "我的博客"))
     var player_level: int = Blogger.level
@@ -15,16 +23,19 @@ func _update_content() -> void:
     var ordinal = 1
     var year = 2005
     var result: Dictionary
+    var award_result_value: int = AwardManager.AwardResult.NONE
 
     if TaskManager and TaskManager.pending_scene_params.has("award_result_data"):
         var award_data = TaskManager.pending_scene_params.award_result_data
         result = award_data
+        award_result_value = result.get("result", AwardManager.AwardResult.NONE)
         if TaskManager.pending_scene_params.has("notification_ordinal"):
             ordinal = TaskManager.pending_scene_params.notification_ordinal
         TaskManager.pending_scene_params.erase("award_result_data")
         TaskManager.pending_scene_params.erase("notification_ordinal")
     elif TaskManager and TaskManager.pending_scene_params.has("test_award_result"):
         var test_result = TaskManager.pending_scene_params.test_award_result
+        award_result_value = test_result
         var test_is_repeat = TaskManager.pending_scene_params.get("test_is_repeat", false)
         if TaskManager.pending_scene_params.has("test_ordinal"):
             ordinal = TaskManager.pending_scene_params.test_ordinal
@@ -40,6 +51,7 @@ func _update_content() -> void:
     else:
         year = YEAR_MAP.get(ordinal, 2005)
         result = AwardManager.process_award(ordinal, year, player_level, player_name)
+        award_result_value = result.get("result", AwardManager.AwardResult.NONE)
 
     var tit_label: Label = get_node_or_null("Panel/tit")
     if tit_label:
@@ -48,6 +60,8 @@ func _update_content() -> void:
     var con_label: Label = get_node_or_null("Panel/con")
     if con_label:
         con_label.text = result.get("content", "")
+
+    _play_confetti_if_celebrated(award_result_value)
 
 func _on_button_pressed() -> void:
     TimerManager.start_timer()
